22c65647339eaf1d5d4267eeca42474ba586d67b
rewks <rewks@users.noreply.github.com>
2023-01-24 20:11:39 +0000

    Rename random id and create storage account

--- main.tf
+++ main.tf
@@ -11,12 +11,12 @@ provider "azurerm" {
   features {}
 }
 
-resource "random_id" "resource_group_id" {
+resource "random_id" "deployment_id" {
     byte_length = 8
 }
 
 resource "azurerm_resource_group" "rg_owa" {
-    name = "rg-OWA-lab-${random_id.resource_group_id.hex}"
+    name = "rg-OWA-lab-${random_id.deployment_id.hex}"
     location = "UK South"
 
     tags = {
@@ -24,3 +24,14 @@ resource "azurerm_resource_group" "rg_owa" {
     }
 }
 
+resource "azurerm_storage_account" "sa_owa" {
+    name = "sa-OWA-lab-${random_id.deployment_id.hex}"
+    resource_group_name = azurerm_resource_group.rg_owa.name
+    location = azurerm_resource_group.rg_owa.location
+    access_tier = "Standard"
+    account_replication_type = "GRS"
+
+    tags = {
+        environment = "test"
+    }
+}
