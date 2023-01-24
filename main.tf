07f271b6f5b54432b7987afe3406e30e47d4d532
rewks <rewks@users.noreply.github.com>
2023-01-24 20:24:55 +0000

    Update virtual network definition

--- main.tf
+++ main.tf
@@ -27,8 +27,13 @@ resource "azurerm_resource_group" "rg_owa" {
 resource "azurerm_virtual_network" "vn_owa" {
     name = "vn-OWA-lab-${random_id.deployment_id.hex}"
     address_space = var.virtual_network_range
+    dns_servers = [var.domain_controller_ip]
     resource_group_name = azurerm_resource_group.rg_owa.name
     location = azurerm_resource_group.rg_owa.location
+
+    tags = {
+        environment = "test"
+    }
 }
 
 resource "azurerm_storage_account" "sa_owa" {
@@ -36,7 +41,7 @@ resource "azurerm_storage_account" "sa_owa" {
     resource_group_name = azurerm_resource_group.rg_owa.name
     location = azurerm_resource_group.rg_owa.location
     access_tier = "Standard"
-    account_replication_type = "GRS"
+    account_replication_type = "LRS"
 
     tags = {
         environment = "test"
--- variables.tf
+++ variables.tf
@@ -4,6 +4,12 @@ variable "virtual_network_range" {
     default = ["10.50.0.0/24"]
 }
 
+variable "domain_controller_ip" {
+    description = "IP to designate to the domain controller"
+    type = string
+    default = "10.50.0.10"
+}
+
 variable "lab_location" {
     description = "Azure location to deploy lab in"
     type = string
