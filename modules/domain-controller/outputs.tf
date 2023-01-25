3b41cbc55285255b0b20c2e2b21e268f0633845f
rewks <rewks@users.noreply.github.com>
2023-01-25 21:24:58 +0000

    Add DC outputs

--- modules/domain-controller/outputs.tf
+++ modules/domain-controller/outputs.tf
@@ -1,3 +1,8 @@
+output "dc_ip" {
+    description = "Public IP for the domain controller"
+    value = azurerm_public_ip.exch_lab_pip_dc.ip_address
+}
+
 output "da_password" {
     description = "Password for the domain admin account"
     value = random_password.DA_password.result
